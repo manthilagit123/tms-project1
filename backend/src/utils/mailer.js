8d661d550c848422fd3502cfd00a45e6c85a3bf6
manthilagit123 <samarak-im23133@stu.kln.ac.lk>
2026-06-27 10:01:50 +0530

    Fix nodemailer hanging on Render Free Tier by adding a 5s connectionTimeout

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 5000,
 });
 
 async function sendWelcomeEmail(toEmail, tempPassword) {
